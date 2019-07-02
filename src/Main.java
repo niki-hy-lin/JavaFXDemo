ee911a615eb54f06e9b8e28ca3027b26a143f336
Huiyin Lin (Niki) <nikcolam@gmail.com>
2019-07-02 15:44:07 -0700

    test

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Title of the Window");
+        button = new Button();
+        button.setText("Click Me");
+
+
+        button.setOnAction(e -> {
+            System.out.println("hey now");
+            System.out.println("hey now 2");
+
+        });
+
+
+
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+
+
+}
